8be0723217f169c6afdb7b6078e6ee2d5a329abe
Pavel1095 <9153220501@mail.ru>
2022-08-02 21:19:03 +0300

    Третье задание выполнено.

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
         for (int spendPerDay : monthArray) {
             sum += spendPerDay;
         }
-            System.out.printf("Сумма трат за месяц составила %s рублей", sum);
-        System.out.println();
+            System.out.printf("Сумма трат за месяц составила %s рублей\n", sum);
         int min = monthArray[0];
         int max = monthArray[0];
         for (int i = 1; i < monthArray.length; i++) {
@@ -17,9 +16,11 @@ public class Main {
                 max = monthArray[i];
             }
         }
-        System.out.printf("Минимальная сумма трат за день составила %s рублей. Максимальная сумма трат за день составилa %s рублей", min, max);
-    }
+        System.out.printf("Минимальная сумма трат за день составила %s рублей. Максимальная сумма трат за день составилa %s рублей\n", min, max);
+        double average = (double) sum / monthArray.length;
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", average);
 
+    }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
